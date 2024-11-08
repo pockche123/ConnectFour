f1302382b60cad78a71dcaf5ac7243d196efc329
Parjal Rai <parjal.rai@dvla.gov.uk>
2024-11-08 16:20:55 +0000

    Create and setup grid

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,58 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class Main {
+
+    private static Scanner stdin = new Scanner(System.in);
+    private static final String[][] grid = new String[7][7];
+    public static void main(String[] args) {
+
+        setUpGrid(grid);
+        printGrid(grid);
+
+        grid[4][1] = "X";
+
+        printGrid(grid);
+
+
+
+
+
+
+
+    }
+
+    public static void setUpGrid(String[][] grid){
+        int num = 1;
+        for(int i=0; i<grid[0].length; i++){
+            grid[0][i] = Integer.toString(num);
+            num += 1;
         }
+
+        for(int r = 1; r< grid.length; r++){
+            for(int c = 0; c<grid[0].length; c++){
+                grid[r][c] = "-";
+            }
+        }
+
+    }
+
+
+
+    public static void printGrid(String[][]grid){
+
+        for(int r = 0; r< grid.length; r++){
+            for(int c = 0; c<grid[0].length; c++){
+                System.out.printf("%s \t",grid[r][c]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("Player 1: X");
+        System.out.println("Player 2: O");
+        System.out.println();
+
     }
 }

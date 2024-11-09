e402aece68376e6b2ab0f7f3a1beb85769915c1f
Parjal Rai <parjal.rai@dvla.gov.uk>
2024-11-09 15:45:10 +0000

    Create basic game functionality

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,56 +13,46 @@ public class Main {
     private static final String[][] grid = new String[7][7];
     public static void main(String[] args) {
 
+        System.out.println();
+        System.out.println("Welcome to the game of Connect Four. Please start the game with player 1 by picking a column.");
+        System.out.println();
         setUpGrid(grid);
-        printGrid(grid);
 
-        System.out.println(grid[5][3]);
-        for(int r=1; r<grid.length; r++){
-            grid[r][2] = "X";
-            grid[r][4] = "X";
+        boolean player1Turn = true;
+        boolean player2Turn = false;
+
+        boolean gameOn = true;
+
+        while(gameOn){
+
+            if(player1Turn){
+                System.out.println("Player 1, please pick a suitable column: ");
+                int[] coordinate = new int[]{-1,-1};
+                while(Arrays.equals(coordinate, new int[]{-1, -1})){
+                    coordinate = pickColumn(Integer.parseInt(stdin.nextLine()), grid, "X");
+                }
+                System.out.printf("Player 1 has picked the column %d. \n", coordinate[1]+1);
+                printGrid(grid);
+                player1Turn = false;
+                player2Turn = true;
+            }
+
+            if(player2Turn){
+                System.out.println("Player 2, please pick a suitable column: ");
+                int[] coordinate = new int[]{-1,-1};
+                while(Arrays.equals(coordinate, new int[]{-1, -1})){
+                    coordinate = pickColumn(Integer.parseInt(stdin.nextLine()), grid, "O");
+                }
+                System.out.printf("Player 2 has picked the column %d. \n", coordinate[1]+1);
+                printGrid(grid);
+                player1Turn = true;
+                player2Turn = false;
+
+            }
+
         }
-        grid[2][2] = "O";
-        grid[2][4] = "O";
-        printGrid(grid);
 
-        timeBombExplode(new int[]{3,3},grid);
-
-
-
-       printGrid(grid);
-       checkForFloatingDiscs(grid,new int[]{3,3});
-
-       printGrid(grid);
-
-//        pickColumn(1,grid,"X");
-//        printGrid(grid);
-//        pickColumn(2,grid,"X");
-//        printGrid(grid);
-//        pickColumn(3,grid,"X");
-//        printGrid(grid);
-//        pickColumn(4,grid,"X");
-//        printGrid(grid);
-//        pickColumn(5,grid,"X");
-//        printGrid(grid);
-//        pickColumn(6,grid,"X");
-//        printGrid(grid);
-//        pickColumn(7,grid,"X");
-//        printGrid(grid);
-//        // Create a diagonal line of 'X's from (1,1) to (4,4)
-//        grid[4][1] = "X";
-//        grid[3][2] = "X";
-//        grid[2][3] = "X";
-//        grid[1][4] = "X";
-
-//        printGrid(grid);
-//
-//        System.out.println(checkWin(grid, "X", new int[]{1,4}));
-//
-//        clearColumn(2,grid);
-//        printGrid(grid);
-//
-//        System.out.println(checkWin(grid, "X", new int[]{1,4}));
-
+//        pickColumn(9,grid,"O");
 
 
     }
@@ -75,13 +66,11 @@ public class Main {
 
         for(int r = 1; r< grid.length; r++){
             for(int c = 0; c<grid[0].length; c++){
-                if(r > 3){
-                    grid[r][c] = "X";
-                }else {
                     grid[r][c] = "-";
                 }
-            }
         }
+        printGrid(grid);
+
 
     }
 
@@ -104,7 +93,7 @@ public class Main {
 
     public static int[] pickColumn(int col, String[][]grid, String disc){
         if(col <= 0 || col > 7){
-            System.err.println("The column has to be within the range of 1 to 7.");
+            System.err.println("Please pick the column within the range of 1 to 7.");
             return new int[]{-1,-1};
         }
 
@@ -115,7 +104,8 @@ public class Main {
             }
         }
 
-            System.out.printf("The column %d is full. Please select another column. \n", col);
+
+            System.out.printf("Column %d is full. Please pick another column. \n", col);
             return new int[]{-1, -1};
 
 

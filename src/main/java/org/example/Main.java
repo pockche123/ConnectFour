c7b7b9e8b307517551d17f40ef4505cc3cc6c156
Parjal Rai <parjal.rai@dvla.gov.uk>
2024-11-12 16:33:10 +0000

    Remove unnessary codes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,5 @@
 package org.example;
 
-import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-
 
 public class Main {
     public static void main(String[] args) {
